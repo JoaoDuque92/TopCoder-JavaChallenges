ebe320fefe642a11b78c99e40ade0796600ee5f8
JoaoDuque92 <30290504+JoaoDuque92@users.noreply.github.com>
2017-07-19 14:59:12 +0100

    Add files via upload

--- challenge/permutible/Permutible.java
+++ challenge/permutible/Permutible.java
@@ -4,21 +4,13 @@ import java.util.Arrays;
 
 public class Permutible {
 
-	
-	public static void main(String[] args) {
-		Permutible p = new Permutible();
-		
-		System.out.println(p.isPossible(100035));
-	}
-	
-	
 	public String isPossible(int n) {
 
-		//We will multiply by 2 to 9 beacause if we multiply by 10 
-		//or more the result will have at least one digit more
-		//and consequently the result will be "Impossible"
+		// We will multiply by 2 to 9 beacause if we multiply by 10
+		// or more the result will have at least one digit more
+		// and consequently the result will be "Impossible"
 		for (int i = 2; i < 10; i++) {
-			if (hasTheSameDigits(n, n * i)){
+			if (hasTheSameDigits(n, n * i)) {
 				return "Possible";
 			}
 		}
